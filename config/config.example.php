233140ed65fce031bfdc0f68e31cb7516a62b5c2
lucassss08_nl <lucaskoolen08@gmail.com>
2025-02-06 14:10:13 +0100

    update

--- config/config.example.php
+++ config/config.example.php
@@ -12,4 +12,4 @@ $dbUser = 'root';
 $dbPass = '';
 
 //De url waarop jouw project staat. Géén slash aan het einde.
-$base_url = 'http://localhost/storingapp';
+$base_url = 'http://stroringapp.test';
